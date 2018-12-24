318a9a9713ecf6e72cb02167a1b7297cd0e19da8
Alexandr Kuntsevich <1st.riddle@gmail.com>
2018-12-24 15:35:47 +0200

    release v1

---
+++ src/test/java/TestForCourses/Pars.java
@@ -0,0 +1,18 @@
+package TestForCourses;
+
+
+import java.util.Arrays;
+
+
+public class Pars {
+
+    public static void main(String[] args) {
+        int[] ints = new int[args.length];
+        for (int i = 0; i < args.length; i++) {
+            ints[i] = Integer.parseInt(args[i]);
+        }
+        Arrays.sort(ints);
+        System.out.println("Array before sort: " + Arrays.toString(args));
+        System.out.println("Array after sort: " + Arrays.toString(ints));
+    }
+}
